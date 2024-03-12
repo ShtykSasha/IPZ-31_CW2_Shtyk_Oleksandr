57cef886a758fa83d60cc01ca5bfa3ff1f7ad75e
KEKES <sashashtyk809@gmail.com>
2024-03-12 14:58:32 +0200

    add functions for switch on sign in success

--- app/src/main/java/com/example/ipz31_cw2_shtyk_oleksandr/ui/theme/SignInActivity.kt
+++ app/src/main/java/com/example/ipz31_cw2_shtyk_oleksandr/ui/theme/SignInActivity.kt
@@ -20,10 +20,52 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        // Знаходимо всі елементи у макеті за їхніми ідентифікаторами
+
         emailEditText = findViewById(R.id.emailEditText)
         passwordEditText = findViewById(R.id.passwordEditText)
         signInButton = findViewById(R.id.signInButton)
         signInSuccessText = findViewById(R.id.signInSuccessText)
         emailText = findViewById(R.id.emailText)
         signOutButton = findViewById(R.id.signOutButton)
+
+
+        signInButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                showSignInSuccess(email)
+            }
+        }
+
+
+        signOutButton.setOnClickListener {
+            showSignIn()
+        }
+    }
+
+
+    private fun showSignIn() {
+        signInSuccessText.visibility = View.GONE
+        emailText.visibility = View.GONE
+        signOutButton.visibility = View.GONE
+        emailEditText.setText("")
+        passwordEditText.setText("")
+        signInButton.visibility = View.VISIBLE
+        emailEditText.visibility = View.VISIBLE
+        passwordEditText.visibility = View.VISIBLE
+    }
+
+
+    private fun showSignInSuccess(email: String) {
+        signInSuccessText.visibility = View.VISIBLE
+        emailText.visibility = View.VISIBLE
+        signOutButton.visibility = View.VISIBLE
+        signInButton.visibility = View.GONE
+        emailEditText.visibility = View.GONE
+        passwordEditText.visibility = View.GONE
+
+
+        emailText.text = "Email: $email"
+    }
+}
